83a5507755b7c8383d7dd9f2465b502568213c4a
“Rahugg” <rahugg07@gmail.com>
2023-02-15 00:16:12 +0600

    "-TodoList: added add buton && listed tasks"

--- lab3/ToDoList/index.js
+++ lab3/ToDoList/index.js
@@ -1,5 +1,16 @@
 document.querySelector('#push').onclick = function () {
     if (document.querySelector('#new-task input').value.length == 0) {
         alert("Kindly Enter Task Name!!!!")
+    } else {
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#new-task input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
     }
 }

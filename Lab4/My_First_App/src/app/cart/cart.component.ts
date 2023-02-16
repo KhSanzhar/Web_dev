47c60a92ea47eb5d3b9578f06cdc35d8ce6e3ec6
“Rahugg” <rahugg07@gmail.com>
2023-02-16 16:45:37 +0600

    "-Angular: Added user input after purchasing"

--- Lab4/My_First_App/src/app/cart/cart.component.ts
+++ Lab4/My_First_App/src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+
 import {CartService} from '../cart.service';
 
 @Component({
@@ -10,9 +12,22 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) {
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
   share() {

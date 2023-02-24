106b3926d0b08d89b25291393596a7947df36f51
Rahugg <rahugg07@gmail.com>
2023-02-24 21:53:36 +0600

    "Lab5 - created App component must show list of categories                 When any category is selected, products of this category should be rendered "

---
+++ lab5/lab5_angular/src/app/categories.ts
@@ -0,0 +1,18 @@
+export interface Category {
+  category: string;
+}
+
+export const categories = [
+  {
+    category: 'Laptops',
+  },
+  {
+    category: 'Phone',
+  },
+  {
+    category: 'TV',
+  },
+  {
+    category: 'Tablets',
+  }
+]

7c6d58d647a4155074809057db38806c4d502bf8
Rahugg <rahugg07@gmail.com>
2023-03-06 14:55:44 +0600

    "Lab6 - created routes for components"

--- lab6/album-web/src/app/app.module.ts
+++ lab6/album-web/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { AlbumsComponentComponent } from './albums-component/albums-component.component';
+import { AlbumDetailComponentComponent } from './album-detail-component/album-detail-component.component';
+import { AlbumPhotosComponentComponent } from './album-photos-component/album-photos-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    AboutComponentComponent,
+    AlbumsComponentComponent,
+    AlbumDetailComponentComponent,
+    AlbumPhotosComponentComponent
   ],
   imports: [
     BrowserModule,

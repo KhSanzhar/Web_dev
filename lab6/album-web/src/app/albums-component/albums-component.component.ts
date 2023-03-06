e296b783adf9dee8c178ca52ef3798b41df8848e
Rahugg <rahugg07@gmail.com>
2023-03-06 16:40:47 +0600

    "Lab6 - created albums component and implemented get request"

--- lab6/album-web/src/app/albums-component/albums-component.component.ts
+++ lab6/album-web/src/app/albums-component/albums-component.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Album} from "../albums";
+import {AlbumsService} from "./albums.service";
 
 @Component({
   selector: 'app-albums-component',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./albums-component.component.css']
 })
 export class AlbumsComponentComponent {
+  albums: Album[];
 
+  constructor(private albumsService: AlbumsService) {
+    this.albums = [];
+  }
+
+  ngOnInit(): void {
+    this.albumsService.getAlbums().subscribe(
+      (response) => {
+        this.albums = response.slice(0, 10);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteAlbum(album: Album): void {
+    const index = this.albums.indexOf(album);
+    if (index !== -1) {
+      this.albums.splice(index, 1);
+    }
+  }
 }

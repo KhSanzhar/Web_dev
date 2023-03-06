ee065a1b0c05e173e6981df70f20ee1daf6a2bbf
Rahugg <rahugg07@gmail.com>
2023-03-06 15:54:12 +0600

    "Lab6 - created header && linked to components"

---
+++ lab6/album-web/src/app/albums-component/albums-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-component',
+  templateUrl: './albums-component.component.html',
+  styleUrls: ['./albums-component.component.css']
+})
+export class AlbumsComponentComponent {
+
+}

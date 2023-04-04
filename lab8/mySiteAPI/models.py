45ff5bad6f1599e31bfe096442462d5ad13f9046
Rahugg <rahugg07@gmail.com>
2023-04-04 11:46:04 +0600

    "Lab 8 - Created Models of Product and Category"

---
+++ lab8/mySiteAPI/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField(null=True, blank=True)
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category': self.category.name
+        }
